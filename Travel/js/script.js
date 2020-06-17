7b5a5b2f53fe6099cb6dd9dedbd9807c6f2c3050
Dmytro <dmitryzhigulin87@gmail.com>
2020-06-17 19:51:53 +0200

    Menu and Form JS

--- Travel/js/script.js
+++ Travel/js/script.js
@@ -6,21 +6,62 @@
         formCheckbox = headerForm.querySelector('[type="checkbox"]'),
         formButton = headerForm.querySelector('.form_button');
 
-
     // console.log(headerForm);
     // console.log(inputField);
     // console.log(formCheckbox);
     // console.log(formButton);
 
-    mainMenu.forEach((item, index) => {
 
-        // console.log(index);
-        // console.log(item);
-        item.addEventListener('click', (event) => {
-            event.preventDefault();
-            console.log(index);
+
+    //функция события 'клик' на основном меню
+    function mainMenuFunc(menu) {
+
+        menu.forEach((item, index) => {
+
+            // console.log(index);
+            // console.log(item);
+            item.addEventListener('click', (event) => {
+                event.preventDefault();
+                console.log(`clicked on ${index + 1}`);
+            });
         });
-    });
+    }
+    mainMenuFunc(mainMenu);
+
+
+
+    //функция проверки корректного заполнения формы хедера
+    function formCheck(form) {
+
+        formButton.addEventListener('click', () => {
+
+            console.log('form button clicked!');
+            let userInfo = [];
+
+            inputField.forEach((item, index) => {
+
+                // console.log(item);
+
+                if ( item.value != '' && item.value != null) {
+
+                    userInfo.push(item.value);
+                    console.log(`done on ${index}`);
+
+                } else {
+                    console.log(`err on ${index}`);
+                    alert("All fields must be filled");
+                }
+
+            });
+    
+            console.log(userInfo);
+
+        });
+
+    }
+    formCheck(headerForm);
+
+    
 
     
 
