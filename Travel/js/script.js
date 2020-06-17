9abe9ba8ab1484028ebd73c2954c3930dedd483a
Dmytro <dmitryzhigulin87@gmail.com>
2020-06-17 18:01:55 +0200

    Menu and Form JS

--- Travel/js/script.js
+++ Travel/js/script.js
@@ -1,7 +1,47 @@
 "use strict";
 
+    const mainMenu = document.querySelectorAll('.main_menu .main_link'),
+        headerForm = document.querySelector('.header_form'),
+        inputField = headerForm.querySelectorAll('.text_field'),
+        formCheckbox = headerForm.querySelector('[type="checkbox"]'),
+        formButton = headerForm.querySelector('.form_button');
 
-alert("Test");
+
+    // console.log(headerForm);
+    // console.log(inputField);
+    // console.log(formCheckbox);
+    // console.log(formButton);
+
+    mainMenu.forEach((item, index) => {
+
+        // console.log(index);
+        // console.log(item);
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            console.log(index);
+        });
+    });
+
+    
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
