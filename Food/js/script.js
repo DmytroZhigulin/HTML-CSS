5e302266e8d824283f709d49cf77042fd85f06ab
Dmytro <dmitryzhigulin87@gmail.com>
2020-06-25 14:39:22 +0300

    Food project 1.0

---
+++ Food/js/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+          tabParent = document.querySelector('.tabheader__items'),
+          tabs = tabParent.querySelectorAll('.tabheader__item');
+
+
+          console.log(tabParent);
+          console.log(tabsContent);
+          console.log(tabs);
+
+
+
+        function hideTabContent() {
+            tabsContent.forEach( item => {
+                item.classList.add('hide');
+                item.classList.remove('show', 'animated');
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+            });
+        }
+        hideTabContent();
+
+
+        function showDefaultTab(i = 0) {
+            tabsContent[i].classList.add('show', 'animated');
+            tabsContent[i].classList.remove('hide');
+            tabs[i].classList.add('tabheader__item_active');
+        }
+        showDefaultTab();
+
+
+
+
+
+        
+
+        tabParent.addEventListener('click', (e) => {
+            
+            const target = e.target;
+
+            if( target && target.classList.contains('tabheader__item')) {
+                tabs.forEach( (item, index) => {
+                    if(target == item) {
+                        hideTabContent();
+                        showDefaultTab(index);
+                    }
+                });
+            }
+          });
+
+        
+
+          
+
+
+});
