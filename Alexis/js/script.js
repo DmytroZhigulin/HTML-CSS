052d62637a7c4e6fe08fe0398b0828a2ceac05b0
Dmytro <dmitryzhigulin87@gmail.com>
2020-06-25 21:51:36 +0300

    2 blocks done

--- Alexis/js/script.js
+++ Alexis/js/script.js
@@ -5,11 +5,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         const mainMenu = document.querySelectorAll('nav>a'),
-              btn = document.querySelector('button');
+              btn = document.querySelector('button'),
+              focusCards = document.querySelectorAll('.ourFocus__card'),
+              portfolioCards = document.querySelectorAll('.portfolio__card');
 
 
         console.log(mainMenu);
         console.log(btn);
+        console.log(focusCards);
+        console.log(portfolioCards);
 
 
         mainMenu.forEach( item => {
@@ -22,7 +26,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
         btn.addEventListener('click', (e) => {
             console.log('OK');
-        })
+        });
 
 
 
