86e7570e7e750a3a48c62c194baa53eb1ba8a218
Dmytro <dmitryzhigulin87@gmail.com>
2020-06-26 22:35:23 +0300

    New blocks

--- Alexis/js/script.js
+++ Alexis/js/script.js
@@ -7,13 +7,16 @@ window.addEventListener('DOMContentLoaded', () => {
         const mainMenu = document.querySelectorAll('nav>a'),
               btn = document.querySelector('button'),
               focusCards = document.querySelectorAll('.ourFocus__card'),
-              portfolioCards = document.querySelectorAll('.portfolio__card');
+              portfolioCards = document.querySelectorAll('.portfolio__card'),
+              playBtn = document.querySelector('.videoSection__pic');
+
 
 
         console.log(mainMenu);
         console.log(btn);
         console.log(focusCards);
         console.log(portfolioCards);
+        console.log(playBtn);
 
 
         mainMenu.forEach( item => {
@@ -24,8 +27,12 @@ window.addEventListener('DOMContentLoaded', () => {
         });
 
 
-        btn.addEventListener('click', (e) => {
+        btn.addEventListener('click', (event) => {
             console.log('OK');
+        });
+
+        playBtn.addEventListener('click', (event) => {
+            console.log('Running video');
         });
 
 
