04a207479435d46129121173ef62bfe7c408bae5
Dmytro <dmitryzhigulin87@gmail.com>
2020-07-02 20:00:54 +0300

    Alexis 3.0

--- Alexis/js/script.js
+++ Alexis/js/script.js
@@ -8,15 +8,19 @@ window.addEventListener('DOMContentLoaded', () => {
               btn = document.querySelector('button'),
               focusCards = document.querySelectorAll('.ourFocus__card'),
               portfolioCards = document.querySelectorAll('.portfolio__card'),
+              teamCards = document.querySelectorAll('.team__cards__card'),
+              teamDescr = document.querySelectorAll('.team_description'),
               playBtn = document.querySelector('.videoSection__pic');
 
 
 
-        console.log(mainMenu);
-        console.log(btn);
-        console.log(focusCards);
-        console.log(playBtn);
-        console.log(portfolioCards);
+        // console.log(mainMenu);
+        // console.log(btn);
+        // console.log(focusCards);
+        // console.log(playBtn);
+        // console.log(portfolioCards);
+        console.log(teamCards);
+        console.log(teamDescr);
         
 
 
@@ -64,6 +68,51 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
         cardsOnHover();
+        
+
+
+        
+
+        function cardDefault() {
+
+            teamDescr.forEach( item => {
+
+                item.classList.add('hide');
+                item.classList.remove('show', 'cardDescrA'); 
+            });
+        }
+        cardDefault();
+        //функция по умолчанию прячет все описания карт 
+
+        function show(i = 0) {
+            
+            teamDescr[i].classList.add('show', 'cardDescrA');
+            teamDescr[i].classList.remove('hide'); 
+                    
+        }
+        show();
+        //функция принимает число i и показывает элемент псевдомассива с картами описания с индексом = i.
+        // 0 по умолчанию.
+
+        function cardShow() {
+
+            teamCards.forEach( (item, i) => {
+                item.addEventListener('click', (e) => {
+                    cardDefault();
+                    show(i);
+                });
+            });
+        }
+        cardShow();
+        // функция перебирает псевдомассив с карточками и на каждую навешивает обработчик события "click",
+        // при клике вызывает функцию показа карточки описания со значением = индексу элемента события "click"
+
+
+            
+
+            
+        
+        
 
 
 
