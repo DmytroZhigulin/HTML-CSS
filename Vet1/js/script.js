541bfe7138d96afeb3ee302d08c1e65c874bfc32
Dmytro <dmitryzhigulin87@gmail.com>
2020-07-14 21:58:48 +0300

    In progres

---
+++ Vet1/js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const header = document.querySelector('header'),
+          preHeaderMenu = header.querySelectorAll('.pre_header__menu > li'),
+          mainMenu = header.querySelectorAll('.main_menu__group'),
+          mainBanScroll = header.querySelectorAll('.banner_scroll'),
+          mainBanImg = header.querySelectorAll('.banner > a'),
+          advantagesCard = document.querySelectorAll('.advantages_card__title'),
+          productCard = document.querySelectorAll('.product_group'),
+          productTitle = document.querySelectorAll('.product_group__title'),
+          buttons = document.querySelectorAll('.btn'),
+          logoScroll = document.querySelectorAll('.logos_acordion > i'),
+          footer = document.querySelector('footer');
+          
+            
+
+    console.log(preHeaderMenu);
+    console.log(mainMenu);
+    console.log(mainBanScroll);
+    console.log(mainBanImg);
+    console.log(advantagesCard);
+    console.log(productCard);
+    console.log(productTitle);
+    console.log(buttons);
+    console.log(footer);
+    console.log(logoScroll);
+    
+
+        
+
+    
+    
+
+
+
+    
+});
