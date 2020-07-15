26f84b841767ac6d9a1f33a40409a528065fca61
Dmytro <dmitryzhigulin87@gmail.com>
2020-07-15 13:36:10 +0300

    Working on sliders

--- Vet1/js/script.js
+++ Vet1/js/script.js
@@ -7,35 +7,61 @@ window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header'),
           preHeaderMenu = header.querySelectorAll('.pre_header__menu > li'),
           mainMenu = header.querySelectorAll('.main_menu__group'),
-          mainBanScroll = header.querySelectorAll('.banner_scroll'),
-          mainBanImg = header.querySelectorAll('.banner > a'),
+          prev = header.querySelector('.left'),
+          next = header.querySelector('.right'),
+          mainBanImg = header.querySelectorAll('.banner_slide'),
           advantagesCard = document.querySelectorAll('.advantages_card__title'),
           productCard = document.querySelectorAll('.product_group'),
           productTitle = document.querySelectorAll('.product_group__title'),
           buttons = document.querySelectorAll('.btn'),
+          logoIcon = document.querySelectorAll('.logos_img'),
           logoScroll = document.querySelectorAll('.logos_acordion > i'),
           footer = document.querySelector('footer');
           
             
 
-    console.log(preHeaderMenu);
-    console.log(mainMenu);
-    console.log(mainBanScroll);
-    console.log(mainBanImg);
-    console.log(advantagesCard);
-    console.log(productCard);
-    console.log(productTitle);
-    console.log(buttons);
-    console.log(footer);
-    console.log(logoScroll);
+    // console.log(preHeaderMenu);
+    // console.log(mainMenu);
+    // console.log(prev);
+    // console.log(next);
+    // console.log(mainBanImg);
+    // console.log(advantagesCard);
+    // console.log(productCard);
+    // console.log(productTitle);
+    // console.log(buttons);
+    // console.log(footer);
+    // console.log(logoIcon);
+    // console.log(logoScroll);
+
+
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+
+        if ( n > mainBanImg.length) {
+            slideIndex = 1;
+        }
+
+        if ( n < 1 ) {
+            slideIndex = mainBanImg.length;
+        }
+
+        mainBanImg.forEach(item => item.style.display = 'none');
+
+        mainBanImg[ slideIndex - 1].style.display = '';
+
+    }
     
+    function plusSlides(n) {
+        showSlides( slideIndex += n )
+    }
 
-        
-
-    
-    
-
-
-
-    
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    }); 
 });
