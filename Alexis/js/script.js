7631a9d77b220374e89be43039630a031f5f766a
Dmytro <dmitryzhigulin87@gmail.com>
2020-09-01 16:58:16 +0300

    classes card

--- Alexis/js/script.js
+++ Alexis/js/script.js
@@ -4,113 +4,147 @@
 window.addEventListener('DOMContentLoaded', () => {
 
 
-        const mainMenu = document.querySelectorAll('nav>a'),
-              btn = document.querySelector('button'),
-              focusCards = document.querySelectorAll('.ourFocus__card'),
-              portfolioCards = document.querySelectorAll('.portfolio__card'),
-              teamCards = document.querySelectorAll('.team__cards__card'),
-              teamDescr = document.querySelectorAll('.team_description'),
-              playBtn = document.querySelector('.videoSection__pic');
+    const mainMenu = document.querySelectorAll('nav>a'),
+            btn = document.querySelector('button'),
+            focusCards = document.querySelectorAll('.ourFocus__card'),
+            portfolioCards = document.querySelectorAll('.portfolio__card'),
+            teamCards = document.querySelectorAll('.team__cards__card'),
+            teamDescr = document.querySelectorAll('.team_description'),
+            playBtn = document.querySelector('.videoSection__pic');
 
 
 
-        // console.log(mainMenu);
-        // console.log(btn);
-        // console.log(focusCards);
-        // console.log(playBtn);
-        // console.log(portfolioCards);
-        console.log(teamCards);
-        console.log(teamDescr);
+    // console.log(mainMenu);
+    // console.log(btn);
+    // console.log(focusCards);
+    // console.log(playBtn);
+    // console.log(portfolioCards);
+    console.log(teamCards);
+    console.log(teamDescr);
         
 
+    // Main Menu
+    mainMenu.forEach( item => {
 
-        mainMenu.forEach( item => {
+        item.addEventListener('click', (e) => {
+            console.log('click');
+        });
+    });
 
+
+    btn.addEventListener('click', (event) => {
+        console.log('OK');
+    });
+
+    playBtn.addEventListener('click', (event) => {
+        console.log('Running video');
+    });
+    
+    // Описание карточек блока OUR TEAM
+    function cardDefault() {
+
+        teamDescr.forEach( item => {
+
+            item.classList.add('hide');
+            item.classList.remove('show', 'cardDescrA'); 
+        });
+    }
+    cardDefault();
+    //функция по умолчанию прячет все описания карт 
+
+    function show(i = 0) {
+        
+        teamDescr[i].classList.add('show', 'cardDescrA');
+        teamDescr[i].classList.remove('hide'); 
+                
+    }
+    show();
+    //функция принимает число i и показывает элемент псевдомассива с картами описания с индексом = i.
+    // 0 по умолчанию.
+
+    function cardShow() {
+
+        teamCards.forEach( (item, i) => {
             item.addEventListener('click', (e) => {
-                console.log('click');
+                cardDefault();
+                show(i);
             });
         });
+    }
+    cardShow();
+    // функция перебирает псевдомассив с карточками и на каждую навешивает обработчик события "click",
+    // при клике вызывает функцию показа карточки описания со значением = индексу элемента события "click"
 
 
-        btn.addEventListener('click', (event) => {
-            console.log('OK');
-        });
+    // Динамическое создание карточек
 
-        playBtn.addEventListener('click', (event) => {
-            console.log('Running video');
-        });
+    //Блок OUR FOCUS
 
-
-        function cardsOnHover() {
-
-            focusCards.forEach( item => {
-
-                item.addEventListener('mouseover', (event) => {
-                    item.classList.add('onHover');
-                });
-                item.addEventListener('mouseout', (event) => {
-                    item.classList.remove('onHover');
-                });
-            });
-
-
-            portfolioCards.forEach( item => {
-
-                item.addEventListener('mouseover', (event) => {
-
-                    item.classList.add('onHover');
-                });
-                item.addEventListener('mouseout', (event) => {
-                    
-                    item.classList.remove('onHover');
-                    
-                });
-            });
+    class focusCard {
+        constructor(src, alt, title, descr, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
         }
-        cardsOnHover();
-        
+
+        render() {
+            const element = document.createElement('div');
 
 
-        
+            if (this.classes.length === 0) {
+                this.element = "ourFocus__card";
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
 
-        function cardDefault() {
 
-            teamDescr.forEach( item => {
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                
+                <h3 class="ourFocus__card__title">${this.title}</h3>
 
-                item.classList.add('hide');
-                item.classList.remove('show', 'cardDescrA'); 
-            });
+                <p class="ourFocus__card__text">
+                    ${this.descr}
+                </p>
+            `;
+            this.parent.append(element);
         }
-        cardDefault();
-        //функция по умолчанию прячет все описания карт 
+    }
+    new focusCard (
+        "../img/web.svg",
+        "picture_1",
+        "Web Design",
+        "Analytics release series A financing launch party interaction design android angel investor.",
+        ".container .ourFocus__Cards"
+    ).render();
+    new focusCard (
+        "../img/design.svg",
+        "picture_2",
+        "UX Design",
+        "Analytics release series A financing launch party interaction design android angel investor.",
+        ".container .ourFocus__Cards"
+    ).render();
+    new focusCard (
+        "../img/photo.svg",
+        "picture_3",
+        "Photogrpahy",
+        "Analytics release series A financing launch party interaction design android angel investor.",
+        ".container .ourFocus__Cards"
+    ).render();
+    new focusCard (
+        "../img/app.svg",
+        "picture_4",
+        "App Development",
+        "Analytics release series A financing launch party interaction design android angel investor.",
+        ".container .ourFocus__Cards"
+    ).render();
 
-        function show(i = 0) {
-            
-            teamDescr[i].classList.add('show', 'cardDescrA');
-            teamDescr[i].classList.remove('hide'); 
-                    
-        }
-        show();
-        //функция принимает число i и показывает элемент псевдомассива с картами описания с индексом = i.
-        // 0 по умолчанию.
 
-        function cardShow() {
-
-            teamCards.forEach( (item, i) => {
-                item.addEventListener('click', (e) => {
-                    cardDefault();
-                    show(i);
-                });
-            });
-        }
-        cardShow();
-        // функция перебирает псевдомассив с карточками и на каждую навешивает обработчик события "click",
-        // при клике вызывает функцию показа карточки описания со значением = индексу элемента события "click"
-
-
-            
-
-            
+    
         
         
 
